c7ae6b0339273f98b5bfb3055b205ba742d963d5
vietnx <viet@102.vn>
2021-09-05 23:00:31 +0700

    Refactor simple - Testnet

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -12,7 +12,7 @@ const pools: PoolConfig[] = [
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
-    tokenPerBlock: '5',
+    tokenPerBlock: '0.01',
     sortOrder: 1,
     isFinished: false,
   },
