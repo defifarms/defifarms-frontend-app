c3706b9af3805f4b0edfbb8ba9a2cd524af7d9aa
PhucLa <laducphuc@gmail.com>
2021-10-01 20:56:28 +0700

    Update exchange ui

--- src/components/Menu/SubNav.tsx
+++ src/components/Menu/SubNav.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link, useLocation } from 'react-router-dom'
-import { ButtonMenu, ButtonMenuItem } from '@pancakeswap/uikit'
+import { ButtonMenu, ButtonMenuItem, lightColors } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
 
 const StyledNav = styled.nav`
@@ -27,11 +27,11 @@ const Nav = () => {
   const { t } = useTranslation()
   return (
     <StyledNav>
-      <ButtonMenu activeIndex={getActiveIndex(location.pathname)} scale="md" variant="primary">
-        <ButtonMenuItem id="swap-nav-link" to="/swap" as={Link}>
+      <ButtonMenu bgColor={lightColors.subNav} fullWidth borderRadius="20px" activeIndex={getActiveIndex(location.pathname)} scale="md" variant="primary">
+        <ButtonMenuItem width="160px" height="72px" borderRadius="20px" id="swap-nav-link" to="/swap" as={Link}>
           {t('Swap')}
         </ButtonMenuItem>
-        <ButtonMenuItem id="pool-nav-link" to="/pool" as={Link}>
+        <ButtonMenuItem width="160px" height="72px" borderRadius="20px" id="pool-nav-link" to="/pool" as={Link}>
           {t('Liquidity')}
         </ButtonMenuItem>
       </ButtonMenu>
