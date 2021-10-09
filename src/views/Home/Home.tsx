502e5326567e6f8b740fb79e4d37a04c702279c2
Nguyễn Văn Dũng <>
2021-10-09 21:46:49 +0700

    Update text Logo, fix color light theme

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -67,7 +67,7 @@ const CardImage = styled.div`
 const BgHome = styled.div`
   border-top-left-radius: ${({ theme }) => theme.radii.homeCorner};
   background-image: url(images/home/7.svg);
-  background-color: rgba(0, 0, 0, 0.45);
+  background-color: rgba(0, 0, 0, ${({ theme }) => theme.isDark ? '0.45' : '0.1'});
   background-blend-mode: multiply;
   background-repeat: no-repeat;
   background-position: bottom center;
--- src/views/Pools/index.tsx
+++ src/views/Pools/index.tsx
@@ -79,7 +79,7 @@ const ControlStretch = styled(Flex)`
 const BgHome = styled.div`
   border-top-left-radius: ${({ theme }) => theme.radii.homeCorner};
   background-image: url(images/home/7.svg);
-  background-color: rgba(0, 0, 0, 0.45);
+  background-color: rgba(0, 0, 0, ${({ theme }) => theme.isDark ? '0.45' : '0.1'});
   background-blend-mode: multiply;
   background-repeat: no-repeat;
   background-position: bottom center;
