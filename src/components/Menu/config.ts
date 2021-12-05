cfb55c7112235e4fdad8d13d02d78689e5524cc6
join-tevet <join_tevet@protonmail.com>
2021-12-05 19:37:25 +0700

    Update menu

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -1,6 +1,5 @@
 import { MenuEntry } from '@defifarms/uikit'
 import { ContextApi } from 'contexts/Localization/types'
-
 const url = process.env.REACT_APP_HOST_1
 const specialUrl = process.env.REACT_APP_HOST_2
 
@@ -8,7 +7,7 @@ const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
   {
     label: t('Home'),
     icon: 'HomeIcon',
-    href: `${url}/`,
+    href: '/',
   },
   {
     label: t('Trade'),
@@ -17,18 +16,18 @@ const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
     items: [
       {
         label: t('Exchange'),
-        href: `${url}/swap`,
+        href: '/swap',
       },
       {
         label: t('Liquidity'),
-        href: `${url}/liquidity`,
+        href: '/liquidity',
       },
     ],
   },
   {
     label: t('Farms'),
     icon: 'FarmIcon',
-    href: `${url}/farms`,
+    href: '/farms',
   },
   {
     label: t('Pools'),
@@ -36,14 +35,96 @@ const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
     items: [
       {
         label: t('Classic Pools'),
-        href: `${url}/pools`,
+        href: '/pools',
       },
       {
         label: t('Special Pools'),
-        href: `${specialUrl}/spools`,
+        href: '/special-pools',
       },
     ],
   },
+  // {
+  //   label: t('Prediction (BETA)'),
+  //   icon: 'PredictionsIcon',
+  //   href: '/prediction',
+  // },
+  // {
+  //   label: t('Lottery'),
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  //   status: {
+  //     text: t('Win').toLocaleUpperCase(),
+  //     color: 'success',
+  //   },
+  // },
+  // {
+  //   label: t('Collectibles'),
+  //   icon: 'NftIcon',
+  //   href: '/collectibles',
+  // },
+  // {
+  //   label: t('Team Battle'),
+  //   icon: 'TeamBattleIcon',
+  //   href: '/competition',
+  // },
+  // {
+  //   label: t('Teams & Profile'),
+  //   icon: 'GroupsIcon',
+  //   items: [
+  //     {
+  //       label: t('Leaderboard'),
+  //       href: '/teams',
+  //     },
+  //     {
+  //       label: t('Task Center'),
+  //       href: '/profile/tasks',
+  //     },
+  //     {
+  //       label: t('Your Profile'),
+  //       href: '/profile',
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: t('Info'),
+  //   icon: 'InfoIcon',
+  //   href: 'https://pancakeswap.info',
+  // },
+  // {
+  //   label: t('IFO'),
+  //   icon: 'IfoIcon',
+  //   href: '/ifo',
+  // },
+  // {
+  //   label: t('More'),
+  //   icon: 'MoreIcon',
+  //   items: [
+  //     {
+  //       label: t('Contact'),
+  //       href: 'https://docs.pancakeswap.finance/contact-us',
+  //     },
+  //     {
+  //       label: t('Voting'),
+  //       href: '/voting',
+  //     },
+  //     {
+  //       label: t('Github'),
+  //       href: 'https://github.com/pancakeswap',
+  //     },
+  //     {
+  //       label: t('Docs'),
+  //       href: 'https://docs.pancakeswap.finance',
+  //     },
+  //     {
+  //       label: t('Blog'),
+  //       href: 'https://pancakeswap.medium.com',
+  //     },
+  //     {
+  //       label: t('Merch'),
+  //       href: 'https://pancakeswap.creator-spring.com/',
+  //     },
+  //   ],
+  // },
   {
     label: t('More'),
     icon: 'MoreIcon',
@@ -77,5 +158,79 @@ const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
     ],
   },
 ]
+
+// const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
+//   {
+//     label: t('Home'),
+//     icon: 'HomeIcon',
+//     href: `${url}/`,
+//   },
+//   {
+//     label: t('Trade'),
+//     icon: 'TradeIcon',
+//     initialOpenState: true,
+//     items: [
+//       {
+//         label: t('Exchange'),
+//         href: `${url}/swap`,
+//       },
+//       {
+//         label: t('Liquidity'),
+//         href: `${url}/liquidity`,
+//       },
+//     ],
+//   },
+//   {
+//     label: t('Farms'),
+//     icon: 'FarmIcon',
+//     href: `${url}/farms`,
+//   },
+//   {
+//     label: t('Pools'),
+//     icon: 'PoolIcon',
+//     items: [
+//       {
+//         label: t('Classic Pools'),
+//         href: `${url}/pools`,
+//       },
+//       {
+//         label: t('Special Pools'),
+//         href: `${specialUrl}/spools`,
+//       },
+//     ],
+//   },
+//   {
+//     label: t('More'),
+//     icon: 'MoreIcon',
+//     initialOpenState: true,
+//     items: [
+//       {
+//         label: t('Contact'),
+//         href: 'https://docs.defifarms.org/contact-us',
+//         target:"_blank"
+//       },
+//       {
+//         label: t('Docs'),
+//         href: 'https://docs.defifarms.org/',
+//         target:"_blank"
+//       },
+//       {
+//         label: 'Github',
+//         href: 'https://github.com/defifarms',
+//         target:"_blank"
+//       },
+//       {
+//         label: 'Blog',
+//         href: 'https://medium.com/@DefiFarmsNFTs',
+//         target:"_blank"
+//       },
+//       {
+//         label: 'Audited by',
+//         href: 'https://callisto.network/defifarms-protocol-security-audit/',
+//         target:"_blank"
+//       },
+//     ],
+//   },
+// ]
 
 export default config
